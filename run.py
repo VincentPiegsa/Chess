23dc02407d813033c805299d8650d858d54607c0
VincentPiegsa <33578820+VincentPiegsa@users.noreply.github.com>
2020-12-06 11:46:13 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -3,4 +3,8 @@ from core import run
 
 if __name__ == '__main__':
 	
+	print('\n======================\n' + 
+		'Chess in Python\nVincent Piegsa © 2020\n' + 
+		'======================\n')
+
 	run()
